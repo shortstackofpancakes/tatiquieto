d1e12abce55101b8c67db764e7b1cd4a0c9b0c1e
shortstackofpancakes <stalin.valerio@hotmail.com>
2016-09-13 18:02:50 -0400

    Create Install.ps1

---
+++ Install.ps1
@@ -0,0 +1,45 @@
+<#
+
+    Tools installation script
+
+#>
+
+
+
+# setup OneGet to use chocolatey
+
+Get-PackageProvider -name chocolatey
+
+
+
+# dev stuff
+
+$chocoPackages = (
+
+	# dev packages
+
+	"firefox",
+
+  "opera",
+
+	"googlechrome",
+
+  "silverlight",
+
+	"greenshot",
+
+	"gimp",
+
+	"7zip.install",
+
+	"vlc",
+
+	"notepadplusplus.install"
+
+);
+
+
+
+# install choco stuff
+
+$chocoPackages | % { Find-Package -Source chocolatey $_ | Install-Package -Verbose -Confirm }
